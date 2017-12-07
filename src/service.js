7af7aec1d43cf11393586e992e64898549e3c232
revan <revansiddh.patil@roxiler.com>
2017-12-07 15:01:45 +0530

    stoped

--- src/service.js
+++
@@ -1,9 +0,0 @@
-import httpService from "./httpService";
-const obj = {
-    doOperation(body) {
-        const url = "https://us-central1-firebase-musimobile.cloudfunctions.net/Count/";
-        return httpService.post(url,body);
-    }
-};
-
-export default obj;
